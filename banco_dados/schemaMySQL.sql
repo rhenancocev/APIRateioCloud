a57367b818ca2d67a4153175e127750761c917f0
Rhenan Cocev Luis <rhenan.luis@sysmap.com.br>
2020-04-14 18:35:29 -0300

    schema do banco

---
+++ banco_dados/schemaMySQL.sql
@@ -0,0 +1,15 @@
+--criando o database
+create database rateiocloud;
+--usando o database
+use rateiocloud;
+--criando a tabela
+CREATE TABLE `CADASTRO_PRODUTO` (
+	`resource_id` varchar(255) NOT NULL,
+	`resource_type` varchar(255) NOT NULL,
+	`resource_name` varchar(255),
+	`projeto` varchar(300),
+	`funcao` varchar(300),
+	`owner_` varchar(300),
+	`rateio` int (1) not null,
+	PRIMARY KEY (`resource_id`)
+);
