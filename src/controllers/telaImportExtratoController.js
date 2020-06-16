5ce8d76f553de1f0ff78e2b6854703def92461dd
Rhenan Cocev Luis <rhenan.luis@sysmap.com.br>
2020-06-16 15:40:23 -0300

    alteração API EXTRATO

--- src/controllers/telaImportExtratoController.js
+++ src/controllers/telaImportExtratoController.js
@@ -24,52 +24,6 @@ exports.deletaDadosTabela = (req,res) => {
 
 }
 
-/*rota para inserir os dados que vem da tela do import CSV
-exports.importCSV2 = (req,res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({ errors: errors.array() })  
-    }else{
-        const produto = {}
-        produto.period                      = req.body.period;
-        produto.account_period              = req.body.account_period;
-        produto.resource_Id                 = req.body.resource_Id;
-        produto.resource_name               = req.body.resource_name;
-        produto.resource_tag                = req.body.resource_tag;
-        produto.be                          = req.body.be;
-        produto.billing_mode                = req.body.billing_mode;
-        produto.fee_name                    = req.body.fee_name;
-        produto.resource_type               = req.body.resource_type;
-        produto.product                     = req.body.product;
-        produto.product_specifications      = req.body.product_specifications;
-        produto.region                      = req.body.region;
-        produto.enterprise_project_id       = req.body.enterprise_project_id;
-        produto.enterprise_project_name     = req.body.enterprise_project_name;
-        produto.usage_type                  = req.body.usage_type;
-        produto.usage_                      = req.body.usage_;
-        produto.usage_type_in               = req.body.usage_type_in;
-        produto.usage_unit                  = req.body.usage_unit;
-        produto.official_total_amount_usd   = req.body.official_total_amount_usd;
-        produto.discount_amount_usd         = req.body.discount_amount_usd;
-        produto.tax_usd                     = req.body.tax_usd;
-        produto.amount_usd                  = req.body.amount_usd;
-        produto.transaction_time            = req.body.transaction_time;
-
-        const sqlQry = 'insert into CLOUD_EXTRATO (period,account_period,resource_Id,resource_name,resource_tag,be,billing_mode,fee_name,resource_type,product,product_specifications,region,enterprise_project_id,enterprise_project_name,usage_type,usage_,usage_type_in,usage_unit,official_total_amount_usd,discount_amount_usd,tax_usd,amount_usd,transaction_time) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
-
-        req.connection.query(sqlQry,[produto.period,produto.account_period,produto.resource_Id,produto.resource_name,produto.resource_tag,produto.be,produto.billing_mode,produto.fee_name,produto.resource_type,produto.product,produto.product_specifications,produto.region,produto.enterprise_project_id,produto.enterprise_project_name,produto.usage_type,produto.usage_,produto.usage_type_in,produto.usage_unit,produto.official_total_amount_usd,produto.discount_amount_usd,produto.tax_usd,produto.amount_usd,produto.transaction_time], (err, result)=>{
-            if(err){
-                console.log(err);
-                res.status(500);
-                res.json({"message":"Internal Server Error"})
-            }else{
-                res.status(201)
-                res.json({"message": result.insertId + " - Dados inseridos com sucesso!"})
-            }
-        })
-    }
-}*/
-
 //-------------------------------------------------------------------------------------//
 
 //rota para inserir os dados que vem da tela do import CSV
@@ -130,7 +84,7 @@ exports.consultarStage = (req,res) => {
 
 }
 
-exports.importCSV2 = (req,res) => {
+exports.importCSV3 = (req,res) => {
     try {
 
         const produto = {}
@@ -164,6 +118,77 @@ exports.importCSV2 = (req,res) => {
             const sqlQry = 'insert into CLOUD_EXTRATO (period,account_period,resource_Id,resource_name,resource_tag,be,billing_mode,fee_name,resource_type,product,product_specifications,region,enterprise_project_id,enterprise_project_name,usage_type,usage_,usage_type_in,usage_unit,official_total_amount_usd,discount_amount_usd,tax_usd,amount_usd,transaction_time) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
 
             req.connection.query(sqlQry,[produto.period,produto.account_period,produto.resource_Id,produto.resource_name,produto.resource_tag,produto.be,produto.billing_mode,produto.fee_name,produto.resource_type,produto.product,produto.product_specifications,produto.region,produto.enterprise_project_id,produto.enterprise_project_name,produto.usage_type,produto.usage_,produto.usage_type_in,produto.usage_unit,produto.official_total_amount_usd,produto.discount_amount_usd,produto.tax_usd,produto.amount_usd,produto.transaction_time], (err,result)=>{
+                
+                if(err){
+                    try {
+                        console.log(err)
+                        return res.status(500).json(err)
+                    } catch (error) {console.log(error)}
+                     
+                }else{
+                    try {
+                        return res.status(201).json({"message": result.insertId + " - Dados inseridos com sucesso!"})
+                    } catch (error){}
+                }
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({"message":"Internal Server Error"})
+    }
+}
+//-------------------------------------------------//
+
+exports.importCSV2 = (req,res) => {
+    try {
+
+        const produto = {}
+        
+        for(var i = 0; i < req.body.cliente.length; i++){
+
+            produto.billing_cycle                       = req.body.cliente[i].billing_cycle;
+            produto.date_                               = req.body.cliente[i].date_;
+            produto.enterprise_project                  = req.body.cliente[i].enterprise_project;
+            produto.enterprise_project_id               = req.body.cliente[i].enterprise_project_id;
+            produto.account_name                        = req.body.cliente[i].account_name;
+            produto.service_type_code                   = req.body.cliente[i].service_type_code;
+            produto.service_type                        = req.body.cliente[i].service_type;
+            produto.resource_type_code                  = req.body.cliente[i].resource_type_code;
+            produto.resource_type                       = req.body.cliente[i].resource_type;
+            produto.billing_mode                        = req.body.cliente[i].billing_mode;
+            produto.bill_type                           = req.body.cliente[i].bill_type;
+            produto.resource_id                         = req.body.cliente[i].resource_id;
+            produto.resource_name                       = req.body.cliente[i].resource_name;
+            produto.resource_tag                        = req.body.cliente[i].resource_tag;
+            produto.resource_specifications             = req.body.cliente[i].resource_specifications;
+            produto.parent_resource_id                  = req.body.cliente[i].parent_resource_id;
+            produto.root_resource_id                    = req.body.cliente[i].root_resource_id;
+            produto.region                              = req.body.cliente[i].region;
+            produto.az                                  = req.body.cliente[i].az;
+            produto.list_price_usd                      = req.body.cliente[i].list_price_usd;
+            produto.discount_usd                        = req.body.cliente[i].discount_usd;
+            produto.amount_usd                          = req.body.cliente[i].amount_usd;
+            produto.cash_payment_usd                    = req.body.cliente[i].cash_payment_usd;
+            produto.cash_coupon_used_usd                = req.body.cliente[i].cash_coupon_used_usd;
+            produto.monthly_settlement_usd              = req.body.cliente[i].monthly_settlement_usd;
+            produto.written_off_usd                     = req.body.cliente[i].written_off_usd;
+
+            const sqlQry = "insert into CLOUD_EXTRATO (billing_cycle,date_,enterprise_project,enterprise_project_id,account_name,service_type_code,service_type,resource_type_code,resource_type,billing_mode,bill_type,resource_id,resource_name,resource_tag,resource_specifications,parent_resource_id,root_resource_id,region,az,list_price_usd,discount_usd,amount_usd,cash_payment_usd,cash_coupon_used_usd,monthly_settlement_usd,written_off_usd) values (?,STR_TO_DATE(?,'%M/%d/%Y'),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+            req.connection.query(sqlQry,
+                [produto.billing_cycle,produto.date_,
+            produto.enterprise_project,produto.enterprise_project_id,
+            produto.account_name,produto.service_type_code,
+            produto.service_type,produto.resource_type_code,
+            produto.resource_type,produto.billing_mode,
+            produto.bill_type,produto.resource_id,
+            produto.resource_name,produto.resource_tag,
+            produto.resource_specifications,produto.parent_resource_id,
+            produto.root_resource_id,produto.region,
+            produto.az,produto.list_price_usd,
+            produto.discount_usd,produto.amount_usd,
+            produto.cash_payment_usd,produto.cash_coupon_used_usd,
+            produto.monthly_settlement_usd,produto.written_off_usd], 
+            (err,result)=>{
                 
                 if(err){
                     try {
