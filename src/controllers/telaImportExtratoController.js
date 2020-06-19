ecc714b0f838b9627f98bcd319d9e1a32fe01e84
Rhenan Cocev Luis <rhenan.luis@sysmap.com.br>
2020-06-18 21:31:04 -0300

    atualizacao da api de import

--- src/controllers/telaImportExtratoController.js
+++ src/controllers/telaImportExtratoController.js
@@ -66,14 +66,17 @@ exports.importCSV = (req,res) => {
             produto.service_type                        = req.body.cliente[i].service_type;
             produto.resource_type_code                  = req.body.cliente[i].resource_type_code;
             produto.resource_type                       = req.body.cliente[i].resource_type;
+            produto.product_id                          = req.body.cliente[i].product_id;
             produto.billing_mode                        = req.body.cliente[i].billing_mode;
             produto.bill_type                           = req.body.cliente[i].bill_type;
             produto.resource_id                         = req.body.cliente[i].resource_id;
             produto.resource_name                       = req.body.cliente[i].resource_name;
             produto.resource_tag                        = req.body.cliente[i].resource_tag;
+            produto.sku_code                            = req.body.cliente[i].sku_code;
             produto.resource_specifications             = req.body.cliente[i].resource_specifications;
             produto.parent_resource_id                  = req.body.cliente[i].parent_resource_id;
             produto.root_resource_id                    = req.body.cliente[i].root_resource_id;
+            produto.region_code                         = req.body.cliente[i].region_code;
             produto.region                              = req.body.cliente[i].region;
             produto.az                                  = req.body.cliente[i].az;
             produto.list_price_usd                      = req.body.cliente[i].list_price_usd;
@@ -84,18 +87,18 @@ exports.importCSV = (req,res) => {
             produto.monthly_settlement_usd              = req.body.cliente[i].monthly_settlement_usd;
             produto.written_off_usd                     = req.body.cliente[i].written_off_usd;
 
-            const sqlQry = "insert into CLOUD_EXTRATO (billing_cycle,date_,enterprise_project,enterprise_project_id,account_name,service_type_code,service_type,resource_type_code,resource_type,billing_mode,bill_type,resource_id,resource_name,resource_tag,resource_specifications,parent_resource_id,root_resource_id,region,az,list_price_usd,discount_usd,amount_usd,cash_payment_usd,cash_coupon_used_usd,monthly_settlement_usd,written_off_usd) values (?,STR_TO_DATE(?,'%M/%d/%Y'),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+            const sqlQry = "insert into CLOUD_EXTRATO (billing_cycle,date_,enterprise_project,enterprise_project_id,account_name,service_type_code,service_type,resource_type_code,resource_type,product_id,billing_mode,bill_type,resource_id,resource_name,resource_tag,sku_code,resource_specifications,parent_resource_id,root_resource_id,region_code,region,az,list_price_usd,discount_usd,amount_usd,cash_payment_usd,cash_coupon_used_usd,monthly_settlement_usd,written_off_usd) values (?,STR_TO_DATE(?,'%M/%d/%Y'),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
 
             req.connection.query(sqlQry,
                 [produto.billing_cycle,produto.date_,
             produto.enterprise_project,produto.enterprise_project_id,
             produto.account_name,produto.service_type_code,
             produto.service_type,produto.resource_type_code,
-            produto.resource_type,produto.billing_mode,
+            produto.resource_type,produto.product_id,produto.billing_mode,
             produto.bill_type,produto.resource_id,
-            produto.resource_name,produto.resource_tag,
+            produto.resource_name,produto.resource_tag,produto.sku_code,
             produto.resource_specifications,produto.parent_resource_id,
-            produto.root_resource_id,produto.region,
+            produto.root_resource_id,produto.region_code,produto.region,
             produto.az,produto.list_price_usd,
             produto.discount_usd,produto.amount_usd,
             produto.cash_payment_usd,produto.cash_coupon_used_usd,
@@ -120,28 +123,7 @@ exports.importCSV = (req,res) => {
     }
 }
 
-//rota filtrar.
-exports.deletaFiltro = (req,res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({ errors: errors.array() })  
-    }else{
-        const sqlQry = 'delete from CLOUD_EXTRATO where amount_usd = 0.00000000 and date_ not between ("2020-05-31") and ("2020-06-02");';
-        req.connection.query(sqlQry, (err, result, rows)=>{
-            if(err){
-                console.log(err);
-                res.status(500);
-                res.json({"message":"Internal Server Error"})
-            }else{
-                res.status(201)
-                res.json({"message": result.insertId + " - Dados deletados com sucesso!"})
-            }
-        })
-    }
-
-}
-
-
+//rota que faz a limpeza do arquivo após importação
 exports.deletaFiltro = (req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
